a21d3dd40a447e6b8ee2fb42ee990f675fc4f985
Lichie <lichie567@protonmail.com>
2021-12-09 18:37:00 -0800

    update to 0.1.3.1

--- LMeter/Plugin.cs
+++ LMeter/Plugin.cs
@@ -25,7 +25,7 @@ namespace LMeter
     {
         public const string ConfigFileName = "LMeter.json";
 
-        public static string Version { get; private set; } = "0.1.3.0";
+        public static string Version { get; private set; } = "0.1.3.1";
 
         public static string ConfigFileDir { get; private set; } = "";
 
