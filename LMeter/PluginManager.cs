afc23fda600f72c18b9f3ff086e2eb48aec5f09f
lichie <lichie567@protonmail.com>
2024-08-02 01:26:45 -0700

    fix fflogs again, cache parser

--- LMeter/PluginManager.cs
+++ LMeter/PluginManager.cs
@@ -156,17 +156,6 @@ namespace LMeter
 
         private void PluginCommand(string command, string arguments)
         {
-            // if (arguments.StartsWith("script"))
-            // {
-            //     string[] split = arguments.Split(" ");
-            //     FFLogsClient? client = Singletons.Get<LogClient>()._fflogsClient;
-            //     if (client is not null && split.Length > 1)
-            //     {
-            //         client.Run(split[1]);
-            //     }
-            //     return;
-            // }
-
             string[] argArray = arguments.Split(" ");
             switch (argArray)
             {
